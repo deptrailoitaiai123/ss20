f568db75c55503f96d586e77af03ae73bac8be58
deptrailoitaiai123 <147390311+deptrailoitaiai123@users.noreply.github.com>
2024-01-11 09:07:27 +0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,10 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s ="Hà Nội"
+
+console.log(i);
+console.log(f);
+console.log(b);
+console.log(s);
+
---
+++ 3.js
@@ -0,0 +1,5 @@
+var width = 5;
+    var height = 5;
+    var dt  = width * height;
+
+   document.write( dt);
